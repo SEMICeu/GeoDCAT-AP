ffd9bf6fb113f915529223595fe566a5f414052b
Andrea <ndr.prg@gmail.com>
2020-12-23 23:56:26 +0100

    Create final version of GeoDCAT-AP v2.0.0

--- releases/2.0.0/examples/rdf-serialiser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in *.rdf; do
-  rdfpipe -o text/turtle "$file" > "$(basename "$file" .rdf).ttl"
-#  rdfpipe -o application/ld+json "$file" > "$(basename "$file" .rdf).jsonld"
-done
-sed -i $'s/    /  /g' *.ttl
-#sed -i $'s/    /  /g' *.jsonld
