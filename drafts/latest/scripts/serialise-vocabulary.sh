31ca2db36e15926425df0e3fe114c9bc27de8e1e
Andrea <ndr.prg@gmail.com>
2021-03-18 09:34:36 +0100

    Editorial fixes

--- drafts/latest/scripts/serialise-vocabulary.sh
+++ drafts/latest/scripts/serialise-vocabulary.sh
@@ -2,7 +2,9 @@
 
 for file in ../*.rdf; do
   rdfpipe -o text/turtle "$file" > "../$(basename "$file" .rdf).ttl"
+  sed -i $'s/    /  /g' "../$(basename "$file" .rdf).ttl"
   rdfpipe -o application/ld+json "$file" > "../$(basename "$file" .rdf).jsonld"
+  sed -i $'s/    /  /g' "../$(basename "$file" .rdf).jsonld"
 done
-sed -i $'s/    /  /g' ../*.ttl
-sed -i $'s/    /  /g' ../*.jsonld
+#sed -i $'s/    /  /g' ../*.ttl
+#sed -i $'s/    /  /g' ../*.jsonld
