b6c3943ec6ff857a099311ffe5e12a5409074a47
unknown <9863263+andrea-perego@users.noreply.github.com>
2023-11-04 20:31:29 +0100

    Update scripts

--- drafts/latest/scripts/serialise-examples.sh
+++ drafts/latest/scripts/serialise-examples.sh
@@ -2,7 +2,7 @@
 
 for file in ../examples/*.rdf; do
   rdfpipe -o text/turtle "$file" > "../examples/$(basename "$file" .rdf).ttl"
-  rdfpipe -o application/ld+json "$file" > "../examples/$(basename "$file" .rdf).jsonld"
+#  rdfpipe -o application/ld+json "$file" > "../examples/$(basename "$file" .rdf).jsonld"
 done
 sed -i $'s/    /  /g' ../examples/*.ttl
-sed -i $'s/    /  /g' ../examples/*.jsonld
+#sed -i $'s/    /  /g' ../examples/*.jsonld
